2d9ba2164aa97f72cfbbba9618150a5e699a24e2
“Andrew <andrewwoods88@gmail.com>
2019-07-30 15:45:29 -0400

    render all the rooms in a list (3:45 )

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Rooms from '../pages/Rooms';
+import Room from '../components/Room';
 
-const RoomsList = () => {
+const RoomsList = ({rooms}) => {
+  
+  if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>no rooms matched your search</h3>
+      </div>    
+    )
+  };
+  
   return (
-    <div>
-      Room list
-    </div>
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {
+          rooms.map(item => {
+            return <Room key={item.id} room={item} />;
+          })
+        }
+      </div>
+    </section>
   );
 };
 
